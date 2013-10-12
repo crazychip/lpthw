bc98ec0dcc45b6d10befb5a4bc3b98337570a79f
crazychip <kjell@skogsrud.net>
2013-10-12 11:34:08 +0200

    with answers

--- ex42.py
+++ ex42.py
@@ -2,71 +2,71 @@
 class Animal(object):
 	pass
 
-## ??
+## Dog is-a Animal
 class Dog(Animal):
 
 	def __init__(self, name):
-		## ??
+		## Dog has attribute name
 		self.name = name
 
-## ??
+## Cat is-a Animal
 class Cat(Animal):
 
 	def __init__(self, name):
-		## ??
+		## has-a attribute name
 		self.name = name
 
-## ??
+## Person is-a object
 class Person(object):
 
 		def __init__(self, name):
-				## ??
+				## has-a name
 				self.name = name
 
 				## Person has-a pet of some kind
 				self.pet = None
 
-## ??
+## Employee is-a Person 
 class Employee(Person):
 
 		def __init__(self, name, salary)
-		## ?? hmm what is this strange magic?
-		super(Employee, self).__init__(name)
-		## ??
-		self.salary = salary
+			## running the __init__ function from its parent. ( hmm what is this strange magic?)
+			super(Employee, self).__init__(name)
+			## employee has-a salary
+			self.salary = salary
 
-## ??
+## Fish is-a object
 class Fish(object):
 	pass
 
-## ??
+## Salmon is-a fish
 class Salmon(Fish):
 		pass
 
-## ??
+## Halibut is-a Fish
 class Halibut(Fish):
 		pass
 
 ## rover is-a dog
 rover = Dog("Rover")
 
-## ??
+## satan is a Cat
 satan = Cat("Satan")
 
-## ??
+## Mary is a Person
 mary = Person("Mary")
 
-## ??
+## Frank is-a Employee
 frank = Employee("Frank", 120000)
 
-## ??
+## Frank has-a pet rover
 frank.pet = rover
 
-## ??
+## flipper is-a fish
 flipper = Fish()
 
-## ?? 
+## crouse is-a Salmon
 crouse = Salmon()
 
-## ??
+## harry is-a Halibut
 harry = Halibut()
